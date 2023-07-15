1c50f46eb57e8034cbbab9d1721bab5abf66b53c
Josiah Parry <josiah.parry@gmail.com>
2023-07-15 10:05:07 -0400

    make freshet distance better and prettier

--- src/rust/src/similarity.rs
+++ src/rust/src/similarity.rs
@@ -6,25 +6,26 @@ use geo::FrechetDistance;
 
 #[extendr]
 fn frechet_distance(x: List, y: List) -> Doubles {
+
     if !x.inherits("rs_LINESTRING") || !y.inherits("rs_LINESTRING") {
         panic!("`x` and `y` but be an `rs_LINESTRING` object")
     }
 
     x
-    .iter()
-    .zip(y.iter())
-    .map(|((_, xi), (_, yi))| {
+        .iter()
+        .zip(y.iter())
+        .map(|((_, xi), (_, yi))| {
 
-        if xi.is_null() || yi.is_null() {
-            Rfloat::na()
-        } else {
-            let l1: LineString = Geom::try_from(xi).unwrap().into();
-            let l2: LineString = Geom::try_from(yi).unwrap().into();
-            l1
-                .frechet_distance(&l2)
-                .into()
-        }
-    }).collect::<Doubles>()
+            if xi.is_null() || yi.is_null() {
+                Rfloat::na()
+            } else {
+                let l1: LineString = Geom::try_from(xi).unwrap().into();
+                let l2: LineString = Geom::try_from(yi).unwrap().into();
+                l1
+                    .frechet_distance(&l2)
+                    .into()
+            }
+        }).collect::<Doubles>()
 }
 extendr_module! {
     mod similarity;
