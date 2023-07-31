85826c226aa1ca91df035744706e8cfd52d8c1cd
Josiah Parry <josiah.parry@gmail.com>
2023-07-31 08:18:31 -0400

    support for point, line, polygon constructors.

--- src/rust/src/construction.rs
+++ src/rust/src/construction.rs
@@ -1,6 +1,6 @@
 use extendr_api::prelude::*;
 use std::collections::HashMap;
-use geo_types::{point, Point, MultiPoint, LineString, coord, Coord};
+use geo_types::{point, Point, MultiPoint, LineString, coord, Coord, Polygon};
 use sfconversions::{vctrs::geom_class, Geom};
 
 trait IsReal {
@@ -101,11 +101,65 @@ fn geom_linestring(x: Doubles, y: Doubles, id: Integers) -> Robj {
 }
 
 
+#[extendr]
+fn geom_polygon(x: Doubles, y: Doubles, id: Integers, ring: Integers) -> Robj {
+    // create empty hash map to store unique vectors of points for each ring
+    let mut map_rings: HashMap<i32, HashMap<i32, Vec<Coord>>> = HashMap::new();
 
+    // iterate through everything and create points
+    for (((xi, yi), idx), ring_idx) in x.iter().zip(y.iter()).zip(id.iter()).zip(ring.iter()) {
+        // check to see if xi and yi are real values / non-missing
+        if xi.is_real() && yi.is_real() {
+            let pnt = coord!(x: xi.inner(), y: yi.inner());
+
+            map_rings
+                .entry(ring_idx.inner())
+                .or_insert(HashMap::new())
+                .entry(idx.inner())
+                .or_insert(Vec::new())
+                .push(pnt);
+        }
+    } 
+
+    // iterate through the hash map to create polygons from each ring
+    let res_vec = map_rings
+        .into_iter()
+        .map(|(_, ring_points)| {
+
+            let polygons = ring_points
+                .into_iter()
+                .map(|(_, pts)| {
+                    LineString::new(pts)
+                })
+                .collect::<Vec<LineString>>();
+
+            let n = polygons.len(); 
+
+            let poly = if n > 1 {
+                let mut piter = polygons.into_iter();
+                let ext = piter.next().unwrap();
+                let inner = piter.collect::<Vec<LineString>>();
+                Polygon::new(ext, inner)
+            } else {
+                Polygon::new(polygons.into_iter().next().unwrap(), vec![])
+            };
+
+            Geom::from(poly)
+
+        })
+        .collect::<Vec<Geom>>();
+
+    // create multipolygon vector
+    List::from_values(res_vec)
+        .set_class(geom_class("polygon"))
+        .unwrap()
+
+}
 
 extendr_module! {
     mod construction;
     fn geom_point;
     fn geom_multipoint;
     fn geom_linestring;
+    fn geom_polygon;
 }
