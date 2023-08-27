c8bd706a2404b9f1a308b8031b92f4c91fd4b6f8
Josiah Parry <josiah.parry@gmail.com>
2023-08-27 12:24:18 -0400

    remove old library

--- R/refactor/utils-constants.R
+++
@@ -1,5 +0,0 @@
-# constants
-rs_classes <- c("rs_POINT", "rs_MULTIPOINT", "rs_POLYGON", "rs_MULTIPOLYGON", "rs_LINESTRING", "rs_MULTILINESTRING")
-
-
-scalar_types <- c("point", "multipoint", "polygon", "multipolygon", "linestring", "multilinestring")
